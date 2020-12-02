a0ecc1972d9756abf6f49b0526d503ef77196251
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2020-12-02 15:50:05 -0300

    Make the episodes enter throw contructor in TableView

--- MyPlayground.playground/Pages/Generic Table View Controller.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Generic Table View Controller.xcplaygroundpage/Contents.swift
@@ -10,10 +10,20 @@ final class EpisodesViewController : UITableViewController {
     var episodes: [Episode] = []
     let reuseIdentifier = "Cell"
     
+    init(episodes: [Episode]) {
+        super.init(style: .plain)
+        self.episodes = episodes
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
     }
 
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return episodes.count
     }
@@ -32,8 +42,8 @@ let sampleEpisodes = [Episode(title: "First Episode"),
                       Episode(title: "Second Episode"),
                       Episode(title: "Third Episode")]
 
-let episodeVC = EpisodesViewController()
-episodeVC.episodes = sampleEpisodes
+let episodeVC = EpisodesViewController(episodes: sampleEpisodes)
+
 episodeVC.view.frame = CGRect(x: 0, y: 0, width: 320, height: 480)
 
 XCPlaygroundPage.currentPage.liveView = episodeVC.view
